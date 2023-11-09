5766321d1ca27578d4851358478fa7d5d2137fc3
Tales Reig Cordova <talesreig03@gmail.com>
2023-11-09 15:25:33 -0300

    firebase ok.

--- Firebase.js
+++ Firebase.js
@@ -3,12 +3,12 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-YiqLdK2FLPLxW_fwxmzpSvMzdBjZkWg",
-  authDomain: "topicosespeciaisemprogramacao.firebaseapp.com",
-  projectId: "topicosespeciaisemprogramacao",
-  storageBucket: "topicosespeciaisemprogramacao.appspot.com",
-  messagingSenderId: "458258591396",
-  appId: "1:458258591396:web:dad6c20a310eb3ac324031"
+  apiKey: "AIzaSyAviO-QuYWTq5t7qH7_l1xQf6EZGF5T6PI",
+  authDomain: "autenticacao-6721f.firebaseapp.com",
+  projectId: "autenticacao-6721f",
+  storageBucket: "autenticacao-6721f.appspot.com",
+  messagingSenderId: "745745208501",
+  appId: "1:745745208501:web:e6d269417f2dcc24a7dafc"
 };
 
 const app = initializeApp(firebaseConfig);
