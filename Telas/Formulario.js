e17df6c2a26092e7334cb5c97a8d99e9819a602c
Tales Reig Cordova <talesreig03@gmail.com>
2023-11-09 09:32:35 -0300

    Criar usuário implementado

--- Telas/Formulario.js
+++ Telas/Formulario.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, Button } from "react-native";
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from '../Firebase'
 
-export default function TelaInicial({ navigation }) {
+export default function Formulario({ navigation }) {
+   const [email, setEmail] = useState('');
+   const [senha, setSenha] = useState('');
+   const [repeatSenha, setRepeatSenha] = useState('');
+
+   const handleSingUp = () => {
+      if (senha == repeatSenha){
+         createUserWithEmailAndPassword(auth, email, senha)
+         .then(() => {
+            navigation.navigate('Inicio')
+         })
+         .catch((error) => {
+            alert(error.message)
+         })
+      }else{
+         alert('As senhas devem ser iguais !!!')
+      }
+   }
+
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 24, marginBottom: 20, fontWeight: 'bold' }}>LOGIN</Text>
       <TextInput
         style={{ width: 300, height: 40, borderColor: 'gray', borderWidth: 1, borderRadius: 5, marginBottom: 10, paddingLeft: 10 }}
         placeholder="Email"
+        onChangeText={(email) => setEmail(email)}
       />
       <TextInput
         style={{ width: 300, height: 40, borderColor: 'gray', borderWidth: 1, borderRadius: 5, marginBottom: 10, paddingLeft: 10 }}
         placeholder="Senha"
         secureTextEntry={true}
+        onChangeText={(senha) => setSenha(senha)}
       />
       <TextInput
         style={{ width: 300, height: 40, borderColor: 'gray', borderWidth: 1, borderRadius: 5, marginBottom: 10, paddingLeft: 10 }}
         placeholder="Repitir Senha"
         secureTextEntry={true}
+        onChangeText={(repeatSenha) => setRepeatSenha(repeatSenha)}
       />
-      <Button title='Cadastrar' color='blue' style={{ width: 100, height: 40, borderRadius: 5, marginBottom: 10, paddingLeft: 10 }} onPress={() => { navigation.navigate('Formulario') }} />
+      <Button title='Cadastrar' color='blue'
+         onPress={handleSingUp}
+      />
       <Button title='Voltar' color='blue' style={{ width: 100, height: 40, borderRadius: 5, marginBottom: 10, paddingLeft: 10 }} onPress={() => { navigation.navigate('Formulario') }} />
     </View>
   );
